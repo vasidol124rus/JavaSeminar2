3892a316877c631756ba0d4d5a3b0fe821eb4cb5
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-08-19 17:14:04 +0700

    Добавил решение 3 задачи

---
+++ Task3/HasTwoAdjacentZeros.java
@@ -0,0 +1,22 @@
+﻿
+public class HasTwoAdjacentZeros {
+
+    public static boolean hasTwoAdjacentZeros(int[] numbers) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] == 0 && numbers[i + 1] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers1 = {1, 2, 0, 0, 3};
+        boolean hasZeros1 = hasTwoAdjacentZeros(numbers1);
+        System.out.println(hasZeros1); // Вывод: true
+
+        int[] numbers2 = {1, 2, 0, 3, 0};
+        boolean hasZeros2 = hasTwoAdjacentZeros(numbers2);
+        System.out.println(hasZeros2); // Вывод: false
+    }
+}
