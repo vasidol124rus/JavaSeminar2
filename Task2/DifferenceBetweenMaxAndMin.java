f82e58cc6b788385c02e18091dd7adf00bbec98f
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-08-19 16:36:21 +0700

    2 задача

---
+++ Task2/DifferenceBetweenMaxAndMin.java
@@ -0,0 +1,25 @@
+﻿
+public class DifferenceBetweenMaxAndMin {
+
+    public static int differenceBetweenMaxAndMin(int[] numbers) {
+        if (numbers.length == 0) {
+            throw new IllegalArgumentException("Массив не должен быть пустым.");
+        }
+
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            } else if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        return max - min;
+    }
+}
+
+    
+
