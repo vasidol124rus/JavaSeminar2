409857f553ff7323e0874ce4afa584d6c9d5a223
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-08-19 16:08:16 +0700

    правка

---
+++ Task1/Main.java
@@ -0,0 +1,23 @@
+﻿public class Main {
+
+    public static int countEvens(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 1, 2, 3, 4};
+        System.out.println(countEvens(nums1)); // 3
+
+        int[] nums2 = {2, 2, 0};
+        System.out.println(countEvens(nums2)); // 3
+
+        int[] nums3 = {1, 3, 5};
+        System.out.println(countEvens(nums3)); // 0
+    }
+}
